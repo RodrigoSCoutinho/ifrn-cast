eb3303d3aff1048b3243d4b75ffd26f867001533
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-05-29 09:27:58 -0300

    adding icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-zinc-800">{children}</body>
+      <body className="bg-zinc-900 text-zinc-50">{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,23 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main></main>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+       <aside className="w-72 bg-zinc-950 p-6">
+        <nav>
+          <a href="">Home</a>
+          <a href="">Search</a>
+          <a href="">Your Library</a>
+        </nav>
+        </aside>
+       <main className="flex-1 p-6">
+        main
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
+      </footer>
+    </div>
 
   )
 }
