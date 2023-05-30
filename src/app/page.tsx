f9aa88d8ef18426df05e7b193de7ea7cbf441b60
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-05-30 10:22:57 -0300

    finished the first part from library music

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight} from 'lucide-react'
+import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play} from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -50,31 +50,58 @@ export default function Home() {
           <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
 
           <div className="grid grid-cols-3 gap-4 mt-4">
-            <a href= "#" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+            <a href= "#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
               <Image src="/foo-fighters.jpg" width={104} height={104} alt="Capa do album Foo Fighters"/>
               <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play/>
+              </button>
+
             </a>
-            <a href= "#" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20">
+            <a href= "#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20">
               <Image src="/foo-fighters.jpg" width={104} height={104} alt="Capa do album Foo Fighters"/>
               <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play/>
+              </button>
             </a>
-            <a href= "#" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20">
+
+            <a href= "#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20">
               <Image src="/foo-fighters.jpg" width={104} height={104} alt="Capa do album Foo Fighters"/>
               <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play/>
+              </button>
             </a>
-            <a href= "#" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20">
+
+            <a href= "#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20">
               <Image src="/foo-fighters.jpg" width={104} height={104} alt="Capa do album Foo Fighters"/>
               <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play/>
+              </button>
             </a>
-            <a href= "#" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20">
+
+            <a href= "#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20">
               <Image src="/foo-fighters.jpg" width={104} height={104} alt="Capa do album Foo Fighters"/>
               <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play/>
+              </button>
             </a>
-            <a href= "#" className="bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20">
+
+            <a href= "#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20">
               <Image src="/foo-fighters.jpg" width={104} height={104} alt="Capa do album Foo Fighters"/>
               <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play/>
+              </button>
             </a>
+
           </div>
+
+          <h2 className="font-semibold text-3xl mt-10">Made for Rodrigo Coutinho</h2>
+
         </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
