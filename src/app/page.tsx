a4fac1c12fe7e89587d1e5f9ed40c17e2e92e2a8
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-05-31 08:09:18 -0300

    finished layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat} from 'lucide-react'
+import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize2} from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -135,7 +135,7 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+      <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
         <div className="flex items-center gap-3">
            <Image src="/foo-fighters.jpg" width={56} height={56} alt="Capa do album Foo Fighters"/>
            <div className="flex flex-col">
@@ -143,7 +143,7 @@ export default function Home() {
             <span className="text-xs text-zinc-400">Foo Fighters</span>
            </div>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-2">
          <div className="flex items-center gap-4">
            <Shuffle size={20} className="text-zinc-200"/>
            <SkipBack  size={20} className="text-zinc-200"/>
@@ -155,12 +155,28 @@ export default function Home() {
         <SkipForward size={20}/>
         <Repeat size={20}/>
          </div>
+
           <div className="flex items-center gap-2">
             <span className="text-xs text-zinc-400">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
             <span className="text-xs text-zinc-400">2:17</span>
           </div>
         </div>
-        <div>
+        <div className="flex items-center gap-4">
+            <Mic2 size={20}/>
+            <LayoutList size={20}/>
+            <Laptop2 size={20}/>
+
+          <div className="flex items-center gap-2">
+            <Volume size={20}/>
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
+            </div>
+          </div>
+
+            <Maximize2 size={20}/>
         </div>
       </footer>
     </div>
